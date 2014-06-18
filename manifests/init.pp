6f0eafbab928bd8c1266793bbf78f74f3a63e38d
Sean Brisbane <s.brisbane1@physics.ox.ac.uk>
2014-06-18 15:11:30 +0100

    Some updates and an example idmapd (non functional)

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,15 +1,22 @@
-class central_file_server {
-   notify { "This class requires a manual keytab to be created with {host,nfs,[cifs]}/$(hostname).physics.ox.ac.uk@PHYSICS.OX.AC.UK, ": }
-   notify { "To do this: create a user in the Users part of the AD, name should be [svcname][hsotname] eg nfsCplxfs3.  Next log on to DC3, open an administrator command prompt, and run ktpass -princ [svc]/fqdn@PHYSICS.OX.AC.UK -mapuser {svcname}{hostname}@PHYSICS.OX.AC.UK -mapop add -out new.keytab.  Secure copy this from DC3 to /etc/krb5.keytab on the new file server" :}
-   class { "central_file_server::generic" :}
-   class { "central_file_server::nfs" :}
-   class { "central_file_server::samba" :}
+class oxford_nfs_server (  )
+inherits oxford_nfs_server::params 
+{
+   notify { "This class requires a manual keytab to be created with {host,nfs,[cifs]}/$(hostname).physics.ox.ac.uk@PHYSICS.OX.AC.UK, " : }
+   notify { "To do this: create a user in the Users part of the AD, name should be [svcname][hsotname] eg nfsCplxfs3.  Next log on to DC3, open an administrator command prompt, and run ktpass -princ [svc]/fqdn@PHYSICS.OX.AC.UK -mapuser {svcname}{hostname}@PHYSICS.OX.AC.UK -mapop add -out new.keytab.  Secure copy this from DC3 to /etc/krb5.keytab on the new file server" : }
+   class { "oxford_nfs_server::generic" : }
+   class { "oxford_nfs_server::nfs" : }
+   class { "oxford_nfs_server::samba" : }
 }
 
-class central_file_server::nfs {
+class oxford_nfs_server::nfs (
+   $secretsfilepath = $oxford_nfs_server::params::secretsfilepath,
+   $configfilepath = $oxford_nfs_server::params::configfilepath
+) inherits oxford_nfs_server::params 
+{
 
-   secretspath = "modules/$module_name"
-   configfilepath = "modules/$module_name"
+
+
+
    $central_nfs_packagelist = ['pam_krb5', 'openldap-clients','nfs-utils', 'nfs4-acl-tools']
    $servicelist = ['nfs', 'rpcgssd', 'rpcsvcgssd' ]
    ensure_packages ( $central_nfs_packagelist )
@@ -18,12 +25,12 @@ class central_file_server::nfs {
                    hasstatus => true,
                    hasrestart => true,
                    enable => true,
-                   require => [Package[$central_nfs_packagelist], Class['central_file_server::generic']],
+                   require => [Package[$central_nfs_packagelist], Class['oxford_nfs_server::generic']],
         }
 
        file { '/etc/sysconfig/nfs':
 
-          source =>  "puppet:///$configfilepath/sysconfig.nfs.central_nfs_server",
+          source =>  "puppet:///$configfilepath/sysconfig.nfs",
           require => Package['nfs-utils'],
           owner   => 'root',
           group   => 'root',
@@ -31,10 +38,23 @@ class central_file_server::nfs {
           notify => Service['nfs']
        }
 
+       file { '/etc/idmapd.conf':
+
+          source =>  "puppet:///$configfilepath/idmapd.conf",
+          require => Package['nfs-utils'],
+          owner   => 'root',
+          group   => 'root',
+          mode    => '0400',
+          notify => Service['nfs']
+      }
        
 }
 
-class central_file_server::generic {
+class oxford_nfs_server::generic   (
+   $secretsfilepath = $oxford_nfs_server::params::secretsfilepath,
+   $configfilepath = $oxford_nfs_server::params::configfilepath
+) inherits oxford_nfs_server::params
+{
       $servicelist = ['nslcd','rpcbind','rpcidmapd']
       $packages =  ['nss-pam-ldapd', 'openldap', 'pam_krb5', 'krb5-libs', 'krb5-workstation']
       ensure_packages ( $packages )
@@ -47,7 +67,7 @@ class central_file_server::generic {
 #Making it secret, it does give a few things away 
     file { '/etc/krb5.conf':
       ensure  => present,
-      source  => "puppet:///$secretsfilepath/krb5.conf.central_file_server",
+      source  => "puppet:///$secretsfilepath/krb5.conf",
       require => Package['pam_krb5'],
       owner   => 'root',
       group   => 'root',
@@ -55,7 +75,7 @@ class central_file_server::generic {
   }
     file { '/etc/nscd.conf':
       ensure  => present,
-      source  => "puppet:///$configfilepath/nscd.conf.central_file_server",
+      source  => "puppet:///$configfilepath/nscd.conf",
       owner   => 'root',
       group   => 'root',
       mode    => '0400',
@@ -63,7 +83,7 @@ class central_file_server::generic {
 
     file { '/etc/nslcd.conf':
       ensure  => present,
-      source  => "puppet:///$secretsfilepath/nslcd.conf.central_file_server",
+      source  => "puppet:///$secretsfilepath/nslcd.conf",
       owner   => 'root',
       group   => 'root',
       mode    => '0400',
@@ -78,7 +98,7 @@ class central_file_server::generic {
    }
    file { '/etc/ldap.conf':
          ensure =>present, 
-         source  => "puppet:///$secretsfilepath/ldap.conf.central_file_server", 
+         source  => "puppet:///$secretsfilepath/ldap.conf", 
          owner   => 'root',
          group   => 'root',
          mode    => '0400',
@@ -86,7 +106,7 @@ class central_file_server::generic {
   }
    file { '/etc/openldap/ldap.conf':
          ensure =>present, 
-         source  => "puppet:///$secretsfilepath/ldap.conf.central_file_server", 
+         source  => "puppet:///$secretsfilepath/ldap.conf", 
          owner   => 'root',
          group   => 'root',
          mode    => '0400',
@@ -96,7 +116,7 @@ class central_file_server::generic {
 
   file {  '/etc/ldap/ldap.conf':
          ensure =>present,
-         source  => "puppet:///$secretsfilepath/ldap.conf.central_file_server",
+         source  => "puppet:///$secretsfilepath/ldap.conf",
          owner   => 'root',
          group   => 'root',
          mode    => '0400',
@@ -114,14 +134,19 @@ class central_file_server::generic {
   #Not that secret, but can give a few things away
    file { '/etc/pam.d/system-auth':
          ensure =>present,
-         source  => "puppet:///$secretsfilepath/pam.d.system-auth.central_file_server",
+         source  => "puppet:///$secretsfilepath/pam.d.system-auth",
          owner   => 'root',
          group   => 'root',
          mode    => '0400',
   }
 
 }
-class central_file_server::samba {
+class oxford_nfs_server::samba ( 
+   $secretsfilepath = $oxford_nfs_server::params::secretsfilepath,
+   $configfilepath = $oxford_nfs_server::params::configfilepath
+) inherits oxford_nfs_server::params
+{
+
    $central_samba_packagelist = ['samba', 'openldap-clients']
    $servicelist = ['nmb','smb']
    ensure_packages ( $central_samba_packagelist )
@@ -134,17 +159,16 @@ class central_file_server::samba {
                    require => Package[$central_samba_packagelist]
     }
 
- #     file { '/etc/samba/smb.conf' : 
- #          source  =>  "samba.smb.conf.central_samba_server", 
-#           owner => 'root',
- #          group => 'root',
-  #         mode => 0444,
-   #   }
       file { '/etc/pam.d/samba' : 
-           source  =>  "puppet:///$secretsfilepath/pam.d.samba.central_samba_server", 
+           source  =>  "puppet:///$secretsfilepath/pam.d.samba", 
            owner => 'root',
            group => 'root',
            mode => 0444,
       }
-
+      file { '/etc/samba/smb.conf' :
+           source => "puppet:///$configfilepath/samba.smb.conf",
+           owner => 'root',
+           group => 'root',
+           mode => 0444,
+      }
 }
